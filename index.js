1eb055801f851025a84e2e2ddb37414eb5615a67
Jennifer Jimenez <jenniferjimenez90@gmail.com>
2022-01-16 12:42:39 -0500

    done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code here!
-document.querySelector("main#main").remove();
+document.querySelector('main#main').remove();
 let newHeader = document.createElement('h1');
-    newHeader.id= "victory";
-    newHeader.innerHTML = "Jennifer is the champion";
-    document.body.append(newHeader)
+newHeader.id = "victory";
+newHeader.textContent = "Jennifer is the champion";
+document.body.append(newHeader);
